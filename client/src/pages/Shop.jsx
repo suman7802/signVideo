b6c7a633323e4c51853a757400df2326be132015
suman7802 <mansu7802@gmail.com>
2024-01-04 15:59:00 +0545

    shop remove repetance

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,18 +1,12 @@
 import {useContext} from 'react';
-import {AuthContext} from '../contexts/Auth.context';
 import {ShopContext} from '../contexts/Shop.context';
 import {Container, Row, Form} from 'react-bootstrap';
 import {FeaturedCoursesContext} from '../contexts/FeaturedCourses.context';
 import ShopItem from '../components/ShopItem';
 
 export default function Shop() {
-  const Auth = useContext(AuthContext);
   const Shop = useContext(ShopContext);
   const FeaturedCourses = useContext(FeaturedCoursesContext);
-
-  if (Auth === undefined) {
-    throw new Error('useAuth must be used within a AuthProvider');
-  }
 
   if (Shop === undefined) {
     throw new Error('useShop must be used within a ShopProvider');
@@ -24,13 +18,11 @@ export default function Shop() {
     );
   }
 
-  const {courses} = Auth;
   const {featuredCourses} = FeaturedCourses;
 
-  const enrolledCourses = courses.map((course) => course.course);
-
   const unsubscribedCourses = featuredCourses.filter(
-    (course) => !enrolledCourses.includes(course.category)
+    (course, index, self) =>
+      index === self.findIndex((t) => t.category === course.category)
   );
 
   return (
