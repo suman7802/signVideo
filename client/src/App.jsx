ef6a63a7f61ac3f1b618c85e8ad007c17eb7c128
suman7802 <mansu7802@gmail.com>
2024-01-07 20:17:50 +0545

    json file edit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import Shop from './pages/Shop';
 import Class from './pages/Class';
 import Login from './pages/Login';
 import Logout from './pages/Logout';
-import Socket from './pages/Socket';
 import Library from './pages/Library';
 import UploadForm from './pages/Upload';
 import NavBar from './components/NavBar';
@@ -26,7 +25,6 @@ export default function App() {
           <ShopContextProvider>
             <ClassCoursesProvider>
               <NavBar />
-              <Socket />
               <UploadProvider>
                 <Routes>
                   <Route index element={<FeaturedCourses />} />
