67ed74687675606cf3ba6b5bcc90d9b4f1876c8f
suman7802 <mansu7802@gmail.com>
2024-04-21 11:35:19 +0545

    console db link

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default async function connectDB() {
+  console.log(process.env.MONGO_URI);
   await mongoose
     .connect(process.env.MONGO_URI)
     .then(console.log(`connected to db`));
