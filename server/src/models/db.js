05a894fe43410f0520cd5e206cd3ebcb34c704e3
suman7802 <mansu7802@gmail.com>
2024-04-21 11:48:17 +0545

    Revert "console db link"
    This reverts commit 67ed74687675606cf3ba6b5bcc90d9b4f1876c8f.

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default async function connectDB() {
-  console.log(process.env.MONGO_URI);
   await mongoose
     .connect(process.env.MONGO_URI)
     .then(console.log(`connected to db`));
